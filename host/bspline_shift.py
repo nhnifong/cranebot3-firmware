d381f46f67e0b19ca0210aa58c14916630e67784
Nathaniel Nifong <naavox@gmail.com>
2025-01-15 16:19:05 -0500

    bit of ai code to try out, probably broken

--- host/bspline_shift.py
+++ host/bspline_shift.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.interpolate import BSpline
-import matplotlib.pyplot as plt
 
 control_points = np.array([
 	[2,5,3],
@@ -14,17 +13,53 @@ control_points = np.array([
 spline_degree = 3
 base_interval = (0, 1)
 clamped_knots = np.array([0,0,0,0,1,1,1,1])
-
 spl = BSpline(clamped_knots, control_points, spline_degree)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-# ax.plot(original_path[:, 0], original_path[:, 1], original_path[:, 2], label="Original Spline (0-1)")
-# ax.plot(extended_path[:, 0], extended_path[:, 1], extended_path[:, 2], label="Extended Spline (0-2)")
-ax.scatter(control_points[:,0], control_points[:,1], control_points[:,2], c='red', label='Control Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_title('B-Spline')
-ax.legend()
-plt.save("foo.png")
+# make a new spline with the exact same knots such that when evaluated over (offset, 1+offset) it produces the same curve as the
+# first one evaluated over (0, 1)
+# the first and last control points of the new spline can be created by sampling the first spline at offsets, but how about the other two?
+
+
+
+## AI code follows
+
+def translate_control_points(control_points, knots, degree, offset):
+    """
+    Computes new control points for a B-spline translation.
+
+    Args:
+        control_points: Original control points.
+        knots: Knot vector.
+        degree: Spline degree.
+        offset: Translation offset.
+
+    Returns:
+        New control points.
+    """
+
+    num_control_points = len(control_points)
+    num_eval_points = num_control_points*10 # Higher than control points for better accuracy
+
+    eval_points = np.linspace(0, 1, num_eval_points)
+    eval_matrix = BSpline.design_matrix(eval_points, knots, degree)
+    
+    shifted_eval_points = eval_points - offset
+    shifted_eval_matrix = BSpline.design_matrix(shifted_eval_points, knots, degree)
+
+    new_control_points = np.linalg.lstsq(eval_matrix, shifted_eval_matrix @ control_points, rcond=None)[0] # Use least squares because eval_matrix is not square
+
+    return new_control_points
+
+offset = 0.5
+new_control_points = translate_control_points(control_points, clamped_knots, spline_degree, offset)
+spl_translated = BSpline(clamped_knots, new_control_points, spline_degree)
+
+import matplotlib.pyplot as plt
+x = np.linspace(0, 1, 100)
+x_translated = np.linspace(offset, 1 + offset, 100)
+
+plt.plot(x, spl(x), label="Original Spline")
+plt.plot(x_translated, spl(x), label="Original Spline shifted")
+plt.plot(x, spl_translated(x), label="Translated Spline")
+plt.legend()
+plt.show()
