8334a5e11b545beb706fe216b99f37e6ae809dca
Nathaniel Nifong <naavox@gmail.com>
2025-02-08 13:32:11 -0500

    raspi anchor server changes

--- raspi-anchor/start_stream.sh
+++ raspi-anchor/start_stream.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rpicam-vid -t 0 --width=2304 --height=1296 --inline --listen -o tcp://0.0.0.0:8888 --codec mjpeg
+rpicam-vid -t 0 --width=2304 --height=1296 --inline --listen -o tcp://0.0.0.0:8888 --codec mjpeg --vflip
