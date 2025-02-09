22ffb2934bb4dafd064fc95dc20f8ec00e44b7ac
Nathaniel Nifong <naavox@gmail.com>
2025-02-08 22:30:53 -0500

    raspi anchor server reads line plan from websocket

--- raspi-anchor/motor_control.py
+++ raspi-anchor/motor_control.py
@@ -12,76 +12,76 @@ STOP = b'\xf7'
 READ_ANGLE = b'\x36'
 
 class MKSSERVO42C:
-	def __init__(self):
-		self.port = serial.Serial ("/dev/ttyAMA0", 38400)
+    def __init__(self):
+        self.port = serial.Serial ("/dev/ttyAMA0", 38400)
 
-	def ping(self):
-		"""
-		Return true if the motor is awake and enabled
-		"""
-		self._sendSingleByteCommand(PING)
-		ans = self.port.read(3)
-		return len(ans) == 3 and ans[1] == b'\x01'
+    def ping(self):
+        """
+        Return true if the motor is awake and enabled
+        """
+        self._sendSingleByteCommand(PING)
+        ans = self.port.read(3)
+        return len(ans) == 3 and ans[1] == b'\x01'
 
-	def stop(self):
-		"""
-		Command the motor to stop immediately.
-		Return true if the motor replied status ok
-		"""
-		self._sendSingleByteCommand(STOP)
-		ans = self.port.read(3)
-		return len(ans) == 3 and ans[1] == b'\x01'
+    def stop(self):
+        """
+        Command the motor to stop immediately.
+        Return true if the motor replied status ok
+        """
+        self._sendSingleByteCommand(STOP)
+        ans = self.port.read(3)
+        return len(ans) == 3 and ans[1] == b'\x01'
 
-	def runConstantSpeed(self, speed):
-		"""
-		Command the motor to run at a constant speed between -127 and +127
-		Return true if the motor replied status ok
-		"""
+    def runConstantSpeed(self, speed):
+        """
+        Command the motor to run at a constant speed between -127 and +127
+        Return true if the motor replied status ok
+        """
 
-		# the first bit is direction
-		if speed > 0:
-			first_bit = 128 # (line lengthening, top of spool moves towards the wall)
-		else:
-			first_bit = 0 # (line shortening, top of spool moves away from the wall)
+        # the first bit is direction
+        if speed > 0:
+            first_bit = 128 # (line lengthening, top of spool moves towards the wall)
+        else:
+            first_bit = 0 # (line shortening, top of spool moves away from the wall)
 
-		# the next 7 bits are speed
-		combined = (min(speed, 127) + first_bit).to_bytes()
+        # the next 7 bits are speed
+        combined = (min(speed, 127) + first_bit).to_bytes()
 
-		message = b'\xe0\xf6' + combined
-		message += self._calculateChecksum(message)
-		self.port.write(message)
-		ans = self.port.read(3)
-		return len(ans) == 3 and ans[1] == b'\x01'
+        message = b'\xe0\xf6' + combined
+        message += self._calculateChecksum(message)
+        self.port.write(message)
+        ans = self.port.read(3)
+        return len(ans) == 3 and ans[1] == b'\x01'
 
-	def getShaftAngle(self):
-		"""
-		Get the absolute shaft angle since boot
-		return (status, result)
-		"""
-		self._sendSingleByteCommand(READ_ANGLE)
-		ans = self.port.read(6) # address byte, 32 bit integer, checksum byte
-		if len(ans) != 6:
-			return False, 0
-		motor_angle = int.from_bytes(ans[1:5], byteorder='big', signed=False)
-		# in the 0 direction, the angle reported by getMotorShaftAngle is decreasing.
-		return True, motor_angle
+    def getShaftAngle(self):
+        """
+        Get the absolute shaft angle since boot
+        return (status, result)
+        """
+        self._sendSingleByteCommand(READ_ANGLE)
+        ans = self.port.read(6) # address byte, 32 bit integer, checksum byte
+        if len(ans) != 6:
+            return False, 0
+        motor_angle = int.from_bytes(ans[1:5], byteorder='big', signed=False)
+        # in the 0 direction, the angle reported by getMotorShaftAngle is decreasing.
+        return True, motor_angle
 
-	def _sendSingleByteCommand(self, b):
-		"""
-		b must be a bytes object with a length of 1
-		"""
-		message = b'\xe0' + b
-		message += self._calculateChecksum(message)
-		self.port.write(message)
+    def _sendSingleByteCommand(self, b):
+        """
+        b must be a bytes object with a length of 1
+        """
+        message = b'\xe0' + b
+        message += self._calculateChecksum(message)
+        self.port.write(message)
 
-	def _calculateChecksum(self, message):
-		"""
-		the last (least signifigant) byte in the sum of all the bytes in the message
-		"""
-		return (sum(message) & 255).to_bytes()
+    def _calculateChecksum(self, message):
+        """
+        the last (least signifigant) byte in the sum of all the bytes in the message
+        """
+        return (sum(message) & 255).to_bytes()
 
 if __name__ == "__main__":
-	motor = MKSSERVO42C()
-	motor.runConstantSpeed(10)
-	sleep(1)
-	motor.stop()
+    motor = MKSSERVO42C()
+    motor.runConstantSpeed(10)
+    sleep(1)
+    motor.stop()
--- raspi-anchor/spools.py
+++ raspi-anchor/spools.py
@@ -7,40 +7,112 @@ DEFAULT_MICROSTEPS = 16
 # A speed of 1 is this many revs/sec
 SPEED1_REVS = 30000.0/(DEFAULT_MICROSTEPS * 200)/60;
 METER_PER_REV = SPOOL_DIAMETER_MM * pi * 0.001;
+DATA_LEN = 1000
+PE_TERM = 1.5
+# maximum acceleration in meters of line per second squared
+MAX_ACCEL = 8.0
+LOOP_DELAY_S = 0.03
+
+def constrain(value, minimum, maximum):
+    return max(minimum, min(value, maximum))
 
 class SpoolController:
-	def __init(self):
-		self.motor = MKSSERVO42C()
-		# Meters of line that were spooled out when zeroAngle was set.
-		self.lineAtStart = 1.9
-		self.zeroAngle = 0
-		# last set speed in motor units (-127 to 127)
-		self.motor_speed = 0
+    def __init(self):
+        self.motor = MKSSERVO42C()
+        # Meters of line that were spooled out when zeroAngle was set.
+        self.lineAtStart = 1.9
+        self.zeroAngle = 0
+        # last set speed in motor units (-127 to 127)
+        self.motor_speed = 0
+        # record of line length. tuples of (time, meters)
+        self.record = []
+        # plan of desired line lengths
+        self.desiredLine = []
+        self.lastIndex = 0
 
-	def setReferenceLength(length):
-		"""
-		Provide an external observation of the current line length
-		"""
-		self.lineAtStart = length
-		self.zeroAngle = self.motor.getShaftAngle()
+    def setReferenceLength(self, length):
+        """
+        Provide an external observation of the current line length
+        """
+        self.lineAtStart = length
+        self.zeroAngle = self.motor.getShaftAngle()
 
-	def currentLineLength(self):
-		"""
-		return current unspooled line in meters
-		"""
-		return METER_PER_REV * (float(self.motor.getShaftAngle()) - self.zeroAngle) / ANGLE_RESOLUTION + self.lineAtStart
+    def setPlan(self, plan):
+        """
+        set plan to an array of tuples of time and length
+        """
+        self.desiredLine = plan
+        self.lastIndex = 0
 
-	async def slowStop(self):
-		direction = self.speed / self.speed
-		while abs(self.speed) > 0:
-			self.speed -= direction
-			self.motor.runConstantSpeed(self.speed)
-			await asyncio.sleep(0.05)
-		self.motor.stop()
+    def popMeasurements(self):
+        copy_record = self.record
+        self.record = []
+        return copy_record
 
-	async def trackingLoop(self):
-		"""
-		Constantly try to match the position and speed given in an array
-		"""
-		while True:
-			await asyncio.sleep(0.03)
+    def currentLineLength(self):
+        """
+        return the curren time and current unspooled line in meters
+        """
+        l = METER_PER_REV * (float(self.motor.getShaftAngle()) - self.zeroAngle) / ANGLE_RESOLUTION + self.lineAtStart
+        # accumulate these so you can send them to the websocket
+        row = (time.time(), l)
+        self.record.append(row)
+        return row
+
+    async def slowStop(self):
+        direction = self.speed / self.speed
+        while abs(self.speed) > 0:
+            self.speed -= direction
+            self.motor.runConstantSpeed(self.speed)
+            await asyncio.sleep(0.05)
+        self.motor.stop()
+
+    async def trackingLoop(self):
+        """
+        Constantly try to match the position and speed given in an array
+        """
+        while True:
+            t, currentLen = self.currentLineLength()
+            
+            if len(self.desiredLine) == 0:
+                if self.speed != 0:
+                    await self.slowStop()
+                continue
+
+            # Find the earliest entry in desiredLine that is still in the future.
+            while self.desiredLine[self.lastIndex][0] <= t:
+                self.lastIndex++
+
+            if self.lastIndex >= DATA_LEN:
+                if self.speed != 0:
+                    await self.slowStop()
+                continue
+
+            targetLen = self.desiredLine[self.lastIndex][1]
+            position_err = targetLen - currentLen
+
+            # What would the speed be between the two datapoints tha straddle the present?
+            # Positive values mean line is lengthening
+            # result is in meters of line per second
+            targetSpeed = ((self.desiredLine[self.lastIdx][1] - self.desiredLine[self.lastIdx-1][1]) 
+                / (self.desiredLine[self.lastIdx][0] - self.desiredLine[self.lastIdx-1][0]))    
+            currentSpeed = self.motor_speed * SPEED1_REVS * METER_PER_REV
+            speed_err = targetSpeed - currentSpeed
+            # If our positional error was zero, we could go exactly that speed.
+            # if our position was behind the targetLen (line is lengthening, and we are shorter than targetLen),
+            # (or line is shortening and we are longer than target len) then we need to go faster than targetSpeed to catch up
+            # ideally we want to catch up in one step, but we have max acceleration constraints.
+            aimSpeed = targetSpeed + position_err * PE_TERM; # meters of line per second
+
+            wouldAccel = (aimSpeed - currentSpeed) / LOOP_DELAY_S
+            if wouldAccel > MAX_ACCEL:
+                aimSpeed = MAX_ACCEL * LOOP_DELAY_S + currentSpeed
+            elif wouldAccel < -MAX_ACCEL:
+                aimSpeed = -MAX_ACCEL * LOOP_DELAY_S + currentSpeed
+
+            # take this speed
+            self.speed = constrain(aimSpeed / meters_per_rev / speed1_revs, -127, 127)
+            self.motor.runConstantSpeed(self.speed)
+
+            await asyncio.sleep(LOOP_DELAY_S)
+
