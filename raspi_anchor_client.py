02a6d10d6020fc2884267ed7e3cd3fcb1027a334
Nathaniel Nifong <naavox@gmail.com>
2025-03-03 22:45:12 -0500

    Add weight sliders

--- raspi_anchor_client.py
+++ raspi_anchor_client.py
@@ -20,12 +20,11 @@ websocket_port = 8765
 def pose_from_det(det):
     return (np.array(det['r'], dtype=float), np.array(det['t'], dtype=float))
 
-# this client is designed for the raspberri pi based anchor
-class RaspiAnchorClient:
-    def __init__(self, address, anchor_num, datastore, to_ui_q, to_pe_q, pool, stat):
+# the genertic client for a raspberri pi based robot component
+class ComponentClient:
+    def __init__(self, address, datastore, to_ui_q, to_pe_q, pool, stat):
         self.address = address
         self.origin_poses = []
-        self.anchor_num = anchor_num # which anchor are we connected to
         self.datastore = datastore
         self.to_ui_q = to_ui_q
         self.to_pe_q = to_pe_q
@@ -33,10 +32,134 @@ class RaspiAnchorClient:
         self.connected = False  # status of connection to websocket
         self.receive_task = None  # Task for receiving messages from websocket
         self.video_task = None  # Task for streaming video
-        self.calibration_mode = False # true is pose calibration mode.
         self.frame_times = {}
         self.pool = pool
         self.stat = stat
+
+    def receive_video(self):
+        # don't connect too early or you will be rejected
+        time.sleep(6)
+        video_uri = f'tcp://{self.address}:{video_port}'
+        print(f'Connecting to {video_uri}')
+        cap = cv2.VideoCapture(video_uri)
+        print(cap)
+        self.conn_status['video'] = True
+        self.notify_video = True
+        while self.connected:
+            last_time = time.time()
+            ret, frame = cap.read()
+            if ret:
+                fnum = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+                try:
+                    timestamp = self.frame_times[fnum]
+                    del self.frame_times[fnum]
+                    now = time.time()
+                    self.stat.latency.append(now - timestamp)
+                    self.stat.framerate.append(1/(now - last_time))
+                    last_time = now
+                except KeyError:
+                    print('received a frame without knowing when it was captured')
+                    continue
+                self.pool.apply_async(locate_markers, (frame,), callback=partial(self.handle_detections, timestamp=timestamp))
+
+    def handle_frame_times(self, frame_time_list):
+        """
+        Handle messages from the server contain information about when frames were captured.
+        this info is not embedded in the video stream, we have to save and reassociate it later.
+        """
+        for ft in frame_time_list:
+            # this item represents the time that rpicam-vid captured the frame with the given number.
+            # we need to know this for when we get frames from the stream
+            if len(self.frame_times) > 500:
+                print('How did we miss 500 frames? Video task crashed?')
+                self.shutdown()
+            self.frame_times[int(ft['fnum'])] = float(ft['time'])
+
+        # do this here because we seemingly can't do it in receive_video
+        if self.notify_video:
+            self.to_ui_q.put({'connection_status': self.conn_status})
+            self.notify_video = False
+
+    async def connect_websocket(self):
+        # main client loop
+        self.conn_status['websocket'] = 1
+        self.conn_status['video'] = False
+        self.to_ui_q.put({'connection_status': self.conn_status})
+        ws_uri = f"ws://{self.address}:{websocket_port}"
+        print(f"Connecting to {ws_uri}...")
+        try:
+            # connect() can be used as an infinite asynchronous iterator to reconnect automatically on errors
+            async for websocket in websockets.connect(ws_uri, max_size=None, open_timeout=10):
+                # try:
+                self.connected = True
+                print(f"Connected to {ws_uri}.")
+                await self.receive_loop(websocket)
+        except asyncio.exceptions.CancelledError:
+            print("Cancelling connection")
+            return
+
+    async def receive_loop(self, websocket):
+        self.conn_status['websocket'] = 2
+        self.to_ui_q.put({'connection_status': self.conn_status})
+        # loop of a single websocket connection.
+        # save a reference to this for send_commands
+        self.websocket = websocket
+        self.notify_video = False
+        # just could not make asyncio deal with this, so I used threading. hey it works, go figure
+        vid_thread = threading.Thread(target=self.receive_video)
+        vid_thread.start()
+        # Loop until disconnected
+        while self.connected:
+            try:
+                message = await websocket.recv()
+                # print(f'received message of length {len(message)}')
+                update = json.loads(message)
+                if 'frames' in update:
+                    self.handle_frame_times(update['frames'])
+                self.handle_update_from_ws(update)
+
+            except Exception as e:
+                # don't catch websockets.exceptions.ConnectionClosedOK because we want it to trip the infinite generator in websockets.connect
+                # so it will stop retrying.
+                print(f"Connection to {self.address} closed.")
+                self.connected = False
+                self.websocket = None
+                self.conn_status['websocket'] = 0
+                self.conn_status['video'] = False
+                self.to_ui_q.put({'connection_status': self.conn_status})
+                raise e
+                break
+        vid_thread.join()
+
+    async def send_commands(self, update):
+        if self.connected:
+            await self.websocket.send(json.dumps(update))
+        # just discard the update if not connected.
+
+    async def slow_stop_spool(self):
+        await self.send_commands({'length_plan' : []})
+
+    async def startup(self):
+        self.ct = asyncio.create_task(self.connect_websocket())
+        await self.ct
+
+    def shutdown(self):
+        # this might get called twice
+        print("\nWait for client shutdown")
+        if self.connected:
+            self.connected = False
+            if self.websocket:
+                asyncio.create_task(self.websocket.close())
+        else:
+            self.ct.cancel()
+
+class RaspiAnchorClient(ComponentClient):
+    def __init__(self, address, anchor_num, datastore, to_ui_q, to_pe_q, pool, stat):
+        super().__init__(address, datastore, to_ui_q, to_pe_q, pool, stat)
+        self.anchor_num = anchor_num # which anchor are we connected to
+        self.conn_status = {'anchor_num': self.anchor_num}
+        self.calibration_mode = False # true is pose calibration mode.
+
         try:
             # read calibration data from file
             saved_info = np.load('anchor_pose_%i.npz' % self.anchor_num)
@@ -55,53 +178,18 @@ class RaspiAnchorClient:
             ('gantry_front', model_constants.gantry_aruco_front_inv, datastore.gantry_pose),
             ('gantry_back', model_constants.gantry_aruco_back_inv, datastore.gantry_pose),
         ]
-
-    def receive_video(self):
-        # don't connect too early or you will be rejected
-        time.sleep(6)
-        video_uri = f'tcp://{self.address}:{video_port}'
-        print(f'Connecting to {video_uri}')
-        cap = cv2.VideoCapture(video_uri)
-        print(cap)
-        self.to_ui_q.put({'connection_status': {
-            'anchor_num': self.anchor_num,
-            'websocket': 2,
-            'video': True,
-        }})
-        while self.connected:
-            last_time = time.time()
-            ret, frame = cap.read()
-            if ret:
-                fnum = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
-                try:
-                    timestamp = self.frame_times[fnum]
-                    del self.frame_times[fnum]
-                    now = time.time()
-                    self.stat.latency.append(now - timestamp)
-                    self.stat.framerate.append(1/(now - last_time))
-                    last_time = now
-                except KeyError:
-                    print('received a frame without knowing when it was captured')
-                    continue
-                self.pool.apply_async(locate_markers, (frame,), callback=partial(self.handle_detections, timestamp=timestamp))
+    def handle_update_from_ws(self, update):
+        if 'line_record' in update and not self.calibration_mode:
+            self.datastore.anchor_line_record[self.anchor_num].insertList(update['line_record'])
 
     def calibrate_pose(self):
         print(f"writing 'anchor_pose_{self.anchor_num}.npz'")
         np.savez(f'anchor_pose_{self.anchor_num}.npz', pose = self.anchor_pose)
         self.to_pe_q.put({'anchor_pose': (self.anchor_num, self.anchor_pose)})
 
-    def handle_frame_times(self, frame_time_list):
-        for ft in frame_time_list:
-            # this item represents the time that rpicam-vid captured the frame with the given number.
-            # we need to know this for when we get frames from the stream
-            if len(self.frame_times) > 500:
-                print('How did we miss 500 frames? Video task crashed?')
-                self.shutdown()
-            self.frame_times[int(ft['fnum'])] = float(ft['time'])
-
     def handle_detections(self, detections, timestamp):
         """
-        handle a list of aruco detections from the server
+        handle a list of aruco detections from the pool
         """
         self.stat.detection_count += len(detections)
         if self.calibration_mode:
@@ -148,85 +236,6 @@ class RaspiAnchorClient:
                         ]))
                         dest.insert(np.concatenate([[timestamp], pose.reshape(6)]))
                         print(f'Inserted pose in datastore name={name} t={detection["s"]}, pose={pose}')
-
-    async def connect_websocket(self):
-        # main client loop
-        ws_uri = f"ws://{self.address}:{websocket_port}"
-        print(f"Connecting to anchor {self.anchor_num} at {ws_uri}...")
-        try:
-            # connect() can be used as an infinite asynchronous iterator to reconnect automatically on errors
-            async for websocket in websockets.connect(ws_uri, max_size=None, open_timeout=10):
-                # try:
-                self.connected = True
-                print(f"Connected to anchor {self.anchor_num} at {ws_uri}.")
-                await self.receive_loop(websocket)
-                # except websockets.exceptions.ConnectionClosed:
-                #     print(f"Connection closed")
-                #     self.connected = False
-                #     continue
-        except asyncio.exceptions.CancelledError:
-            print("Cancelling connection")
-            return
-
-    async def receive_loop(self, websocket):
-        self.to_ui_q.put({'connection_status': {
-            'anchor_num': self.anchor_num,
-            'websocket': 2,
-            'video': True,
-        }})
-        # loop of a single websocket connection.
-        # save a reference to this for send_anchor_commands_async
-        self.websocket = websocket
-        # just could not make asyncio deal with this, so I used threading. hey it works, go figure
-        vid_thread = threading.Thread(target=self.receive_video)
-        vid_thread.start()
-        # Loop until disconnected
-        while self.connected:
-            try:
-                message = await websocket.recv()
-                # print(f'received message of length {len(message)}')
-                update = json.loads(message)
-                if 'line_record' in update and not self.calibration_mode:
-                    self.datastore.anchor_line_record[self.anchor_num].insertList(update['line_record'])
-                if 'frames' in update:
-                    self.handle_frame_times(update['frames'])
-
-            except Exception as e:
-                # don't catch websockets.exceptions.ConnectionClosedOK because we want it to trip the infinite generator in websockets.connect
-                # so it will stop retrying.
-                print(f"Connection to anchor {self.anchor_num} closed.")
-                self.connected = False
-                self.websocket = None
-                self.to_ui_q.put({'connection_status': {
-                    'anchor_num': self.anchor_num,
-                    'websocket': 0,
-                    'video': False,
-                }})
-                raise e
-                break
-        vid_thread.join()
-
-    async def send_commands(self, update):
-        if self.connected:
-            await self.websocket.send(json.dumps(update))
-        # just discard the update if not connected.
-
-    async def slow_stop_spool(self):
-        await self.send_commands({'length_plan' : []})
-
-    async def startup(self):
-        self.ct = asyncio.create_task(self.connect_websocket())
-        await self.ct
-
-    def shutdown(self):
-        # this might get called twice
-        print("\nWait for client shutdown")
-        if self.connected:
-            self.connected = False
-            if self.websocket:
-                asyncio.create_task(self.websocket.close())
-        else:
-            self.ct.cancel()
 
 if __name__ == "__main__":
     from multiprocessing import Queue
--- raspi_gripper_client.py
+++ raspi_gripper_client.py
@@ -1,173 +1,21 @@
 
-import asyncio
-import os
-import signal
-import websockets
-import time
-import json
-from cv_common import locate_markers, compose_poses, invert_pose, average_pose
-import cv2
-import numpy as np
-import model_constants
-from functools import partial
-import threading
+from raspi_anchor_client import ComponentClient
 
 # number of origin detections to average
 video_port = 8888
 websocket_port = 8765
 
-def pose_from_det(det):
-    return (np.array(det['r'], dtype=float), np.array(det['t'], dtype=float))
-
-# this client is designed for the raspberri pi based gripper
-class RaspiGripperClient:
+class RaspiGripperClient(ComponentClient):
     def __init__(self, address, datastore, to_ui_q, to_pe_q, pool, stat):
-        self.address = address
-        self.datastore = datastore
-        self.to_ui_q = to_ui_q
-        self.to_pe_q = to_pe_q
-        self.websocket = None
-        self.connected = False  # status of connection to websocket
-        self.receive_task = None  # Task for receiving messages from websocket
-        self.video_task = None  # Task for streaming video
-        self.frame_times = {}
-        self.pool = pool
-        self.stat = stat
+        super().__init__(address, datastore, to_ui_q, to_pe_q, pool, stat)
+        self.conn_status = {'gripper': True}
 
-    def receive_video(self):
-        # don't connect too early or you will be rejected
-        time.sleep(6)
-        video_uri = f'tcp://{self.address}:{video_port}'
-        print(f'Connecting to {video_uri}')
-        cap = cv2.VideoCapture(video_uri)
-        print(cap)
-        self.to_ui_q.put({'connection_status': {
-            'gripper': True,
-            'websocket': 2,
-            'video': True,
-        }})
-        while self.connected:
-            last_time = time.time()
-            ret, frame = cap.read()
-            if ret:
-                fnum = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
-                try:
-                    timestamp = self.frame_times[fnum]
-                    del self.frame_times[fnum]
-                    now = time.time()
-                    self.stat.latency.append(now - timestamp)
-                    self.stat.framerate.append(1/(now - last_time))
-                    last_time = now
-                except KeyError:
-                    print('received a frame without knowing when it was captured')
-                    continue
-                self.pool.apply_async(locate_markers, (frame,), callback=partial(self.handle_detections, timestamp=timestamp))
-
-    def handle_frame_times(self, frame_time_list):
-        for ft in frame_time_list:
-            # this item represents the time that rpicam-vid captured the frame with the given number.
-            # we need to know this for when we get frames from the stream
-            if len(self.frame_times) > 500:
-                print('How did we miss 500 frames? Video task crashed?')
-                self.shutdown()
-            self.frame_times[int(ft['fnum'])] = float(ft['time'])
+    def handle_update_from_ws(self, update):
+        if 'line_record' in update:
+            self.datastore.winch_line_record.insertList(update['line_record'])
 
     def handle_detections(self, detections, timestamp):
         """
-        handle a list of aruco detections from the server
+        handle a list of aruco detections from the pool
         """
-        self.stat.detection_count += len(detections)
-        for detection in detections:
-            pass
-
-    async def connect_websocket(self):
-        # main client loop
-        ws_uri = f"ws://{self.address}:{websocket_port}"
-        print(f"Connecting to gripper at {ws_uri}...")
-        try:
-            # connect() can be used as an infinite asynchronous iterator to reconnect automatically on errors
-            async for websocket in websockets.connect(ws_uri, max_size=None, open_timeout=10):
-                # try:
-                self.connected = True
-                print(f"Connected to gripper at {ws_uri}.")
-                await self.receive_loop(websocket)
-        except asyncio.exceptions.CancelledError:
-            print("Cancelling connection")
-            return
-
-    async def receive_loop(self, websocket):
-        self.to_ui_q.put({'connection_status': {
-            'gripper': True,
-            'websocket': 2,
-            'video': False,
-        }})
-        # loop of a single websocket connection.
-        self.websocket = websocket
-        # just could not make asyncio deal with this, so I used threading. hey it works, go figure
-        vid_thread = threading.Thread(target=self.receive_video)
-        vid_thread.start()
-        # Loop until disconnected
-        while self.connected:
-            try:
-                message = await websocket.recv()
-                # print(f'received message of length {len(message)}')
-                update = json.loads(message)
-                if 'line_record' in update:
-                    self.datastore.winch_line_record.insertList(update['line_record'])
-                if 'frames' in update:
-                    self.handle_frame_times(update['frames'])
-
-            except Exception as e:
-                # don't catch websockets.exceptions.ConnectionClosedOK because we want it to trip the infinite generator in websockets.connect
-                # so it will stop retrying.
-                print("Connection to gripper closed.")
-                self.connected = False
-                self.websocket = None
-                self.to_ui_q.put({'connection_status': {
-                    'gripper': True,
-                    'websocket': 0,
-                    'video': False,
-                }})
-                raise e
-                break
-        vid_thread.join()
-
-    async def send_commands(self, update):
-        if self.connected:
-            await self.websocket.send(json.dumps(update))
-        # just discard the update if not connected.
-
-    async def slow_stop_spool(self):
-        await self.send_commands({'length_plan' : []})
-
-    async def startup(self):
-        self.ct = asyncio.create_task(self.connect_websocket())
-        await self.ct
-
-    def shutdown(self):
-        # this might get called twice
-        print("\nWait for client shutdown")
-        if self.connected:
-            self.connected = False
-            if self.websocket:
-                asyncio.create_task(self.websocket.close())
-        else:
-            self.ct.cancel()
-
-if __name__ == "__main__":
-    from multiprocessing import Queue
-    from data_store import DataStore
-    datastore = DataStore(horizon_s=10, n_cables=4)
-    to_ui_q = Queue()
-    to_pe_q = Queue()
-    to_ob_q = Queue()
-    to_ui_q.cancel_join_thread()
-    to_pe_q.cancel_join_thread()
-    to_ob_q.cancel_join_thread()
-
-    async def main():
-        ac = RaspiGripperClient("127.0.0.1", datastore, to_ui_q, to_pe_q, None)
-        loop = asyncio.get_running_loop()
-        loop.add_signal_handler(getattr(signal, 'SIGINT'), ac.shutdown)
-        await ac.startup()
-    asyncio.run(main())
+        pass
