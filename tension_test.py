a27dd5822e008bef239c2b60ab76632942b41fdc
Nathaniel Nifong <naavox@gmail.com>
2025-03-28 14:15:03 -0400

    Live testing tension equalizer on one anchor

--- tension_test.py
+++ tension_test.py
@@ -3,21 +3,22 @@ import time
 
 m = MKSSERVO42C()
 
-def runUntilLevel(level)
+def runUntilLevel(speed, level):
 	_, raw = m.getShaftError()
 	smoothed = raw
 	for i in range(30):
 		_, raw = m.getShaftError()
 		smoothed = smoothed * 0.9 + raw * 0.1
-		print(f'raw={aerr} smoothed={smoothed}')
+		print(f'raw={raw} smoothed={smoothed}')
 		time.sleep(1/30)
-	m.runConstantSpeed(-1)
-	while aerr < level:
+	m.runConstantSpeed(speed)
+	while abs(smoothed) < level:
 		try:
 			_, raw = m.getShaftError()
-			smoothed = smoothed * 0.9 + raw * 0.1
-			print(f'raw={aerr} smoothed={smoothed}')
+			smoothed = smoothed * 0.95 + raw * 0.05
+			print(f'raw={raw} smoothed={smoothed}')
 			time.sleep(1/30)
 		except KeyboardInterrupt:
 			m.runConstantSpeed(0)
+			break
 	m.runConstantSpeed(0)
