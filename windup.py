b7e619af3a8f23157ed5500d9c559a4b23437e8e
Nathaniel Nifong <naavox@gmail.com>
2025-10-11 22:44:19 -0400

    windup

--- windup.py
+++ windup.py
@@ -9,17 +9,16 @@ full_diameter=model_constants.full_spool_diameter_power_line
 full_length=model_constants.assumed_full_line_length
 gear_ratio=20/51
 sc = SpiralCalculator(empty_diameter, full_diameter, full_length, gear_ratio, -1)
-revs = sc.
 
 motor = MKSSERVO42C()
 assert(motor.ping())
 
 try:
 	revs = motor.getShaftAngle()
-	finish_revs = sc.calc_za_from_length(full_length, revs)
+	finish_revs = sc.calc_za_from_length(0, revs)
 	while revs > finish_revs:
 		print(f'revs={revs} finish={finish_revs}')
-		motor.runConstantSpeed(-1)
+		motor.runConstantSpeed(-4)
 		time.sleep(0.2)
 		revs = motor.getShaftAngle()
 finally:
