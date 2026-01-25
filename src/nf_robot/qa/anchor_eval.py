5a2e0c5ac0771a9e97296450f2c858c6ad08b66b
Nathaniel Nifong <naavox@gmail.com>
2026-01-25 18:18:27 -0500

    anchor qa script sets power anchor type

--- src/nf_robot/qa/anchor_eval.py
+++ src/nf_robot/qa/anchor_eval.py
@@ -1,54 +1,66 @@
 # script to wind up the correct length of line on on an anchor
 import time
 import RPi.GPIO as GPIO
+import argparse
 
 import nf_robot.common.definitions as model_constants
 from nf_robot.robot.mks42c_motor import MKSSERVO42C
 from nf_robot.robot.spools import SpiralCalculator
 
-with open('server.conf', 'r') as file:
-    for line in file:
-        line = line.strip()  # Remove leading/trailing whitespace
-        if not line.startswith('#') and line:  # Check if line is not a comment and is not empty
-            component_type = line
-            print(f'determined type {component_type}')
-            break
+def main():
 
-if component_type == 'anchor':
-	full_diameter=model_constants.full_spool_diameter_fishing_line
-elif component_type == 'power anchor':
-	full_diameter=model_constants.full_spool_diameter_power_line
+	parser = argparse.ArgumentParser(description="Stringman Component")
+	parser.add_argument(
+	        'anchor_type',
+	        type=str,
+	        default='anchor',
+	        choices=['anchor', 'power_anchor'],
+	        help="The type of anchor to set (choices: anchor, power_anchor)"
+	    )
+	args = parser.parse_args()
 
-empty_diameter=model_constants.empty_spool_diameter
-full_length=model_constants.assumed_full_line_length
-gear_ratio=20/51
-sc = SpiralCalculator(empty_diameter, full_diameter, full_length, gear_ratio, -1)
+	# Write the file that differentiates power anchors from regular anchors
+	if args.anchor_type == 'anchor':
+		full_diameter=model_constants.full_spool_diameter_fishing_line
+	elif args.anchor_type == 'power anchor':
+		full_diameter=model_constants.full_spool_diameter_power_line
 
-motor = MKSSERVO42C()
-assert(motor.ping())
+	with open('/opt/robot/server.conf', 'w') as f:
+		f.write('args.anchor_type' + '\n')
 
-SWITCH_PIN = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	empty_diameter=model_constants.empty_spool_diameter
+	full_length=model_constants.assumed_full_line_length
+	gear_ratio=20/51
+	sc = SpiralCalculator(empty_diameter, full_diameter, full_length, gear_ratio, -1)
 
-print("Click switch to begin winding spool")
-count = 0
-tight = False
-while count < 50 and not tight:
-    time.sleep(0.1)
-    tight = GPIO.input(SWITCH_PIN) == 0
-assert tight, 'switch never registered any click'
-print('switch functioning normally, begin winding')
+	motor = MKSSERVO42C()
+	assert(motor.ping())
 
-try:
-	_, revs = motor.getShaftAngle()
-	finish_revs = sc.calc_za_from_length(0, revs)
-	while revs > finish_revs:
-		print(f'revs={revs} finish={finish_revs}')
-		motor.runConstantSpeed(-4)
-		time.sleep(0.2)
+	SWITCH_PIN = 18
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+	print("Click switch to begin winding spool")
+	count = 0
+	tight = False
+	while count < 50 and not tight:
+	    time.sleep(0.1)
+	    tight = GPIO.input(SWITCH_PIN) == 0
+	assert tight, 'switch never registered any click'
+	print('switch functioning normally, begin winding')
+
+	try:
 		_, revs = motor.getShaftAngle()
-finally:
-	motor.runConstantSpeed(0)
+		finish_revs = sc.calc_za_from_length(0, revs)
+		while revs > finish_revs:
+			print(f'revs={revs} finish={finish_revs}')
+			motor.runConstantSpeed(-4)
+			time.sleep(0.2)
+			_, revs = motor.getShaftAngle()
+	finally:
+		motor.runConstantSpeed(0)
 
-print('test complete. camera not checked.')
+	print('test complete. camera not checked.')
+
+if __name__ == "__main__":
+    main()
