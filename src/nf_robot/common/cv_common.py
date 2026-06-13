034d9339de6295c5f5b70c111c125b14d655a811
Nathaniel Nifong <naavox@gmail.com>
2026-06-12 21:51:10 -0400

    toys tag was being ignored

--- src/nf_robot/common/cv_common.py
+++ src/nf_robot/common/cv_common.py
@@ -28,7 +28,7 @@ MARKER_NAMES = [
 ] # next tag id 14
 
 CAL_MARKERS = set(['origin', 'cal_assist_1', 'cal_assist_2', 'cal_assist_3'])
-OTHER_MARKERS = set(['gamepad', 'hamper', 'trash', 'gamepad_back', 'hamper_back', 'trash_back'])
+OTHER_MARKERS = set(['gamepad', 'hamper', 'trash', 'toys', 'gamepad_back', 'hamper_back', 'trash_back', 'toys_back'])
 
 # AprilTag images are typically downloaded, not generated in code.
 # We are using the tag36h11 tag family.
